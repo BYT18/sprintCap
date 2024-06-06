c89cf951fe2fe0f01523511fac2ec3fd24f30c5c
Brandon Tang <Bytang18@gmail.com>
2024-06-06 12:33:30 -0400

    new requirments

--- src/backend/mocap/settings.py
+++ src/backend/mocap/settings.py
@@ -127,12 +127,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-#STATIC_URL = 'static/'
+STATIC_URL = 'static/'
+
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'build/static'),
-]
+#STATIC_URL = '/static/'
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, 'build/static'),
+#]
 
 
 CORS_ALLOWED_ORIGINS = [
