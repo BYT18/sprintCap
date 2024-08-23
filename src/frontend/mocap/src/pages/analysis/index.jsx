8a7c29f2f02bbf55c904a0e160247bd7ec720b09
Brandon Tang <Bytang18@gmail.com>
2024-08-23 04:19:54 -0400

    changed links

--- src/frontend/mocap/src/pages/analysis/index.jsx
+++ src/frontend/mocap/src/pages/analysis/index.jsx
@@ -309,8 +309,8 @@ const loadImage = (src) => {
             // Create the POST request using the fetch API
             const token = localStorage.getItem('access_token');
             console.log(token)
-            const response = await fetch('http://127.0.0.1:8000/test/', {
-            //const response = await fetch('http://3.131.119.69:8000/test/', {
+            //const response = await fetch('http://127.0.0.1:8000/test/', {
+            const response = await fetch('http://3.131.119.69:8000/test/', {
                 method: 'POST',
                 headers: {
                 'Authorization': `Bearer ${token}` // Include the Authorization header
